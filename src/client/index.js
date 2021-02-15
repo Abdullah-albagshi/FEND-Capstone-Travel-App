8d46b85a99730de4764c3d52730a081839e4f491
Abdullah albagshi <abdullah.albagshi@gmail.com>
2021-02-15 21:57:55 +0300

    feat: import bootstrap

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,13 @@
 import { app } from './js/app';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/bootstrap/dist/js/bootstrap.min'
+import '../../node_modules/jquery/dist/jquery.min'
+import '../../node_modules/popper.js/dist/popper.min'
+import '../../node_modules/bootstrap-select/dist/css/bootstrap-select.min.css'
+import '../../node_modules/bootstrap-select/dist/js/bootstrap-select.min'
 
 import './styles/styles.scss';
+
+
 
 export { app };
