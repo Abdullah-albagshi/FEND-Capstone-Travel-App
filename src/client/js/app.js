6f70a15945a846a8f85a9ea359a186f015acdaaf
Abdullah albagshi <abdullah.albagshi@gmail.com>
2021-02-16 23:21:36 +0300

    feat: add remove, save, load functions, .. , add alerts for wrong inputs, add info  about trip such as img days left and weather.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,18 +3,26 @@ import { geoNames } from './API/geonamesAPI';
 import { weather } from './API/weatherbitAPI';
 import { pixabay } from './API/pixabayAPI';
 
-addCountryToSelect();
 
-const moment = require('moment')
-const btn = document.querySelector('.submit');
+const createBtn = document.querySelector('.create');
+const saveBtn = document.querySelector('.save');
+const loadBtn = document.querySelector('.load');
+const removeBtn = document.querySelector('.remove');
+
+const alerts = document.querySelector('.alerts');
+const tripInfo = document.querySelector('#trip-info');
+const tripWeatherImage = document.querySelector('.trip-weather-image');
 const select = document.querySelector('.selectpicker');
 const carouselItem = document.querySelector('.carousel-inner');
 const accordion = document.querySelector('#collapseOne .card-body');
+const loader = document.getElementById('loader');
 
 const startDate = document.querySelector('#start-date');
 const endDate = document.querySelector('#end-date');
 const today = new Date();
-let cityName, lat, lon;
+
+let cityName, lat, lon, carouselItemSlide, accordionItem, tripInfoSave;
+let isTrip = false;
 
 function addCountryToSelect() {
     countries().then((data) => {
@@ -26,17 +34,22 @@ function addCountryToSelect() {
     });
 }
 
-btn.addEventListener('click', async() => {
+createBtn.addEventListener('click', async() => {
+    remove();
+
     if (checkDate() == false) {
-        return
+        return;
     } else {
-        checkDate()
+        checkDate();
     }
-    console.log('test');
+    loader.classList.add('loader');
     let inputCity = select.value.split(' ').join('+');
     let geoData = await geoNames(inputCity);
     if (!geoData) {
-        console.log('fail'); //TODO: handel city not found
+        alerts.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>city not found!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>`;
+        loader.classList.remove('loader');
         return;
     }
     lat = geoData.lat;
@@ -44,144 +57,180 @@ btn.addEventListener('click', async() => {
     cityName = geoData.name.split(' ').join('+');
     let weatherData = await weather({ lat, lon });
     let pixabayData = await pixabay(cityName);
+    await tripDescription(geoData);
     await addItemsToAccordion(weatherData);
     await addItemsToCarousel(pixabayData);
-    console.log(weatherData);
-    console.log(pixabayData);
+    loader.classList.remove('loader');
+    isTrip = true;
 });
+
+function tripDescription(geoData) {
+    let daysTillTrip = parseInt(
+        (startDate.valueAsDate - today) / (1000 * 60 * 60 * 24) + 1
+    );
+    let tripDuration = parseInt(
+        (endDate.valueAsDate - startDate.valueAsDate) / (1000 * 60 * 60 * 24) +
+        1
+    );
+
+    tripInfoSave = ` <h4>The country you want to visit is <strong>${
+		geoData.countryName || geoData.name
+	}</strong> </h4>
+    <h4>Departing on : <strong> ${startDate.value}</strong></h4>
+    <h4><strong> ${daysTillTrip}</strong> day(s) Till the trip</h4>
+    <h4>The trip duration: <strong> ${tripDuration} </strong> day(s)</h4>`;
+
+    tripInfo.innerHTML = tripInfoSave;
+    tripInfo.classList.remove('d-none');
+}
 
 function checkDate() {
     let startDateObj = new Date(startDate.valueAsDate);
     let endDateObj = new Date(endDate.valueAsDate);
 
     if (!startDate.value || !endDate.value || !select.value) {
-        //TODO : alert for empty dates
-        console.log('test in in ');
+        alerts.innerHTML = `<div class="alert alert-warning  alert-dismissible fade show" role="alert">
+        <strong>All Fields must be field!</strong></div>`;
         return false;
     } else {
-        //TODO : alert for empty dates
-
+        alerts.innerHTML = '';
     }
-    //86400000
+    //86400000 is a day in milliseconds
     if (startDateObj - today < -86400000) {
-        console.log(startDateObj - today);
-        //TODO : alert for start day must be larger than today
-        return false
+        alerts.innerHTML = `<div class="alert alert-danger  alert-dismissible fade show " role="alert">
+        <strong>The start date can't be less today date!</strong></div>`;
+        return false;
     }
-
     if (endDateObj - startDateObj < 0) {
-        //TODO : alert end date can't be less than start date 
-        console.log(endDateObj - startDateObj);
+        alerts.innerHTML = `<div class="alert alert-danger  alert-dismissible fade show" role="alert">
+        <strong>end date can't be less than start date!</strong></div>`;
         return false;
     }
 }
 
 function addItemsToCarousel(data) {
-    /*      <div class="carousel-item active">
-                <img class="d-block w-100" src="..." alt="First slide">
-            </div>
- */
-    let items = '';
+    carouselItemSlide = '';
+    if (!data.hits.length) {
+        carouselItemSlide += `<div class="carousel-item active">
+        <img class="d-block carousel-img" src="https://bitsofco.de/content/images/2018/12/broken-1.png" alt="errorSlinde" onerror="this.onerror=null;this.src='https://bitsofco.de/content/images/2018/12/broken-1.png';">
+    </div>`;
+        carouselItem.innerHTML = carouselItemSlide;
+        tripWeatherImage.classList.remove('d-none');
+        return;
+    }
     data.hits.forEach((item, index) => {
-        items += `<div class="carousel-item">
+        carouselItemSlide += `<div class="carousel-item">
         <img class="d-block carousel-img" src="${item.webformatURL}" alt="${
 			index + 1
-		} slide" onerror="this.onerror=null;this.src='../assets/img/imagefound.png';">
+		} slide" onerror="this.onerror=null;this.src='https://bitsofco.de/content/images/2018/12/broken-1.png';">
     </div>`;
     });
-    carouselItem.innerHTML = items;
+    carouselItem.innerHTML = carouselItemSlide;
     carouselItem.firstElementChild.classList.add('active');
+    tripWeatherImage.classList.remove('d-none');
 }
 
 function addItemsToAccordion(weather) {
     let startDateObj = new Date(startDate.valueAsDate);
     let endDateObj = new Date(endDate.valueAsDate);
-    let items = ''
-    let counter = 0
-    weather.data.forEach((day, index) => {
-        let date = new Date(day.datetime)
+    accordionItem = '';
+    let counter = 0;
+    weather.data.forEach((day) => {
+        let date = new Date(day.datetime);
 
         if (date >= startDateObj && date <= endDateObj) {
-            items += `<div class="card">
+            accordionItem += `<div class="card mb-2">
             <div class="card-header" id="heading${counter}">
                 <h2 class="mb-0">
-                    <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse${counter}" aria-expanded="false" aria-controls="collapse${counter}">
-                  Weather for Day ${counter+1} at ${day.datetime}
-                </button>
+                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapse${counter}" aria-expanded="false" aria-controls="collapse${counter}">
+                Weather for Day ${counter + 1} at ${day.datetime}</button>
                 </h2>
             </div>
             <div id="collapse${counter}" class="collapse" aria-labelledby="heading${counter}" data-parent="#collapseOne">
                 <div class="card-body">
-                    <p>Minimum temperature is ${day.min_temp}</p>
-                    <p>Max temperature is ${day.max_temp}</p>
-                    <p>Average temperature is ${day.temp}</p>
-                    <p>The Weather is mostly ${day.weather.description} 
-                    <img src="https://www.weatherbit.io/static/img/icons/${day.weather.icon}.png">
-                    </p>
-                    
-                    
+                <p>${day.weather.description} </p>
+                <img src="https://www.weatherbit.io/static/img/icons/${
+					day.weather.icon
+				}.png">
+                <p>Minimum temperature is <strong> ${
+					day.min_temp
+				}&deg;</strong></p>
+                <p>Average temperature is <strong> ${
+					day.temp
+				}&deg;</strong> </p>
+                    <p>Max temperature is <strong>${
+						day.max_temp
+					}&deg;</strong> </p>
                 </div>
             </div>
-        </div>`
+        </div>`;
             counter++;
         }
-
-    })
+    });
     if (counter === 0) {
-        items += `
-        <h3>The Weather For the selected Date is not available but</h3>
+        accordionItem += `
+        <div class="alert alert-warning" role="alert">The Weather For the selected Date is not available but</div>
         <div class="card">
         <div class="card-header" id="headingToday">
             <h2 class="mb-0">
                 <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseToday" aria-expanded="false" aria-controls="collapseToday">
               The Weather for Today at ${weather.data[0].datetime}
+              
             </button>
             </h2>
         </div>
         <div id="collapseToday" class="collapse" aria-labelledby="headingToday" data-parent="#collapseOne">
             <div class="card-body">
-                <p>Minimum temperature is ${weather.data[0].min_temp}</p>
-                <p>Max temperature is ${weather.data[0].max_temp}</p>
-                <p>Average temperature is ${weather.data[0].temp}</p>
-                <p>The Weather is mostly ${weather.data[0].weather.description} 
-                <img src="https://www.weatherbit.io/static/img/icons/${weather.data[0].weather.icon}.png">
-                </p>
+            <p>${weather.data[0].weather.description} </p>
+            <img src="https://www.weatherbit.io/static/img/icons/${weather.data[0].weather.icon}.png">
+                <p>Minimum temperature is <strong> ${weather.data[0].min_temp}&deg;</strong></p>
+                <p>Average temperature is <strong> ${weather.data[0].temp}&deg;</strong> </p>
+                <p>Max temperature is <strong>${weather.data[0].max_temp}&deg;</strong> </p>
             </div>
         </div>
-    </div>`
+    </div>`;
     }
-    accordion.innerHTML = items
-
-    /*          <div class="accordion" id="accordionDate">
-                    <div class="card">
-                        <div class="card-header" id="headingOne">
-                            <h2 class="mb-0">
-                                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-										Collapsible Group Item #1
-									</button>
-                            </h2>
-                        </div>
-
-                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionDate">
-                            <div class="card-body">
-                                <div class="card">
-                                    <div class="card-header" id="headingTwo">
-                                        <h2 class="mb-0">
-                                            <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                          Collapsible Group Item #2
-                                        </button>
-                                        </h2>
-                                    </div>
-                                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#collapseOne">
-                                        <div class="card-body">
-                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-                                            on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
-                                            occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> */
+    accordion.innerHTML = accordionItem;
 }
+
+saveBtn.addEventListener('click', () => {
+    if (!carouselItemSlide || !accordionItem || !tripInfoSave) {
+        alerts.innerHTML = `<div class="alert alert-danger  alert-dismissible fade show " role="alert">
+        <strong>You can't save without creating a trip</strong></div>`;
+        return false;
+    }
+    localStorage.setItem('carouselItemSlide', carouselItemSlide);
+    localStorage.setItem('accordionItem', accordionItem);
+    localStorage.setItem('tripInfoSave', tripInfoSave);
+});
+
+loadBtn.addEventListener('click', () => {
+    if (!localStorage.getItem('tripInfoSave') ||
+        !localStorage.getItem('accordionItem') ||
+        !localStorage.getItem('carouselItemSlide')
+    ) {
+        alerts.innerHTML = `<div class="alert alert-danger  alert-dismissible fade show " role="alert">
+        <strong>There is no saved trip</strong></div>`;
+        return false;
+    }
+    tripInfo.innerHTML = localStorage.getItem('tripInfoSave');
+    carouselItem.innerHTML = localStorage.getItem('carouselItemSlide');
+    accordion.innerHTML = localStorage.getItem('accordionItem');
+    carouselItem.firstElementChild.classList.add('active');
+    tripInfo.classList.remove('d-none');
+    tripWeatherImage.classList.remove('d-none');
+});
+
+removeBtn.addEventListener('click', remove);
+
+function remove() {
+    tripWeatherImage.classList.add('d-none');
+    tripInfo.classList.add('d-none');
+    localStorage.clear();
+
+    carouselItem.innerHTML = '';
+    accordion.innerHTML = '';
+    tripInfo.innerHTML = '';
+}
+
+export { addCountryToSelect, tripDescription, checkDate, addItemsToCarousel, addItemsToAccordion, remove }
